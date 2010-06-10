35422c235e372f1e6ae49f964f0a5eaf9e21ecd3
Matt Kern <mkern@artisantechnologies.com>
2010-06-10 11:48:40 -0400

    Refrain from sending emails unless the river went up compared to the last check.

---
+++ test/watchcreek_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require '../watchcreek.rb'
+
+class WatchcreekTest < Test::Unit::TestCase
+  
+  def test_truth
+    true
+  end
+  
+end
--- watchcreek.rb
+++ watchcreek.rb
@@ -1,49 +1,66 @@
 #!/usr/bin/env ruby
 
- # =====================================================================================
- # = Fetch the current flow levels on Tumalo Creek and alert above a certain threshold =
- # =====================================================================================
- 
- require 'open-uri'
- require 'rubygems'
- require 'hpricot'
- require 'tmail'
- require 'net/smtp'
- 
- URL = "http://www.usbr.gov/pn-bin/rtgraph.pl/?sta=TUMO&parm=Q"
- MAIL_TO = "matt@thekerns.net"
- MAIL_CC = ""
- MAIL_FROM = "riverkeeper@thekerns.net"
- SUBJECT = "*** Neighborhood River Watch ***"
- 
- pattern = /Last Value =\s+(\d+\.\d+)/
- severities = [{:label => "over flood stage", :value => 350}, {:label => "at flood stage", :value =>  250}, {:label => "getting fucking high", :value => 130}]
- 
- 
- open(URL) do |f|
-   puts "opening url"
-   doc = Hpricot(f)
-   res = doc.at("center").inner_html
-   #puts "result is #{res}"
-   res =~ pattern
-   puts "value is #{$1.to_f}"
-   severities.each do |s|
-     puts "testing against value: #{s[:value].to_f}"
-     if $1.to_f >= s[:value].to_f 
-       puts "River is #{s[:label]}"
-       mail = TMail::Mail.new
-       message = "At #{$1} CFS creek flow is #{s[:label]}. \n\nSee #{URL} for details."
-       mail.body = message
-       mail.to = MAIL_TO
-       mail.from = MAIL_FROM
-       mail.cc = MAIL_CC
-       mail.subject = SUBJECT
-       mail.date = Time.now
-       puts "Mail body is: \n #{mail.body}"
-       Net::SMTP.start('localhost', 25) do |smtp|
-         smtp.send_message(mail.to_s, mail.from, mail.to)
-       end
-       break
-     end
-   end
- end
+# =====================================================================================
+# = Fetch the current flow levels on Tumalo Creek and alert above a certain threshold =
+# =====================================================================================
+if $0 == __FILE__
+
+  require 'open-uri'
+  require 'rubygems'
+  require 'hpricot'
+  require 'tmail'
+  require 'net/smtp'
+
+  URL = "http://www.usbr.gov/pn-bin/rtgraph.pl/?sta=TUMO&parm=Q"
+  MAIL_TO = "matt@thekerns.net"
+  MAIL_CC = ""
+  MAIL_FROM = "riverkeeper@thekerns.net"
+  SUBJECT = "*** Neighborhood River Watch ***"
+  PATTERN = /Last Value =\s+(\d+\.\d+)/
+  SEVERITIES = [{:label => "over flood stage", :value => 350}, {:label => "at flood stage", :value =>  250}, {:label => "getting fucking high", :value => 130}]
+  TMPDIR = "/tmp"
+  
+  def extract_cfs(doc)
+    res = doc.at("center").inner_html
+    #puts "result is #{res}"
+    res =~ PATTERN
+    puts "value is #{$1.to_f}"
+    $1.to_f
+  end
+  
+  def last_cfs
+    File.open(File.join(TMPDIR, 'watchcreek'), 'r') do |f|
+      @last_cfs = f.readline
+    end
+    return @last_cfs.to_f
+  end
+
+  open(URL) do |f|
+    puts "opening url"
+    @doc = Hpricot(f)
+  end
+  cfs = extract_cfs(@doc)
+  SEVERITIES.each do |s|
+    puts "testing against value: #{s[:value].to_f}"
+    if cfs.to_f >= s[:value].to_f
+      File.open(File.join(TMPDIR, 'watchcreek'), 'w') do |f|
+        f.puts cfs
+      end
+      exit if last_cfs >= cfs
+      mail = TMail::Mail.new
+      message = "At #{cfs} CFS creek flow is #{s[:label]}. \n\nSee #{URL} for details."
+      mail.body = message
+      mail.to = MAIL_TO
+      mail.from = MAIL_FROM
+      mail.cc = MAIL_CC
+      mail.subject = SUBJECT
+      mail.date = Time.now
+      puts "Mail body is: \n #{mail.body}"
+      Net::SMTP.start('localhost', 25) do |smtp|
+        smtp.send_message(mail.to_s, mail.from, mail.to)
+      end
+      break
+    end
+  end
+end
+
